0b3c327e2f0810e1f927298c3df6115c7da92cd2
Jacob <jacobryanschreiber@gmail.com>
2024-11-06 17:21:30 -0500

    useEffect created for target word

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [targetWord, setTargetWord] = useState("");
+  const [guessWord, setGuessWord] = useState("");
+  const [guessAttemptsLeft, setguessAttemptsLeft] = useState(6);
+  const [square, setSquare] = useState();
+  const [feedback, setFeedback] = useState("");
+
+  const words = ["bloom", "crazy", "diner"];
+
+  useEffect(() => {
+    const word = words[Math.floor(Math.random() * words.length)];
+    setTargetWord(word);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Wordle</h1>
+      {targetWord}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
